607f4a393875797a354f7797366d22a07a8101b4
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2022-06-30 12:53:48 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import "express-async-errors";
+import cors from "cors";
 import express from "express";
 import { appRoutes } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 
 appRoutes(app);
